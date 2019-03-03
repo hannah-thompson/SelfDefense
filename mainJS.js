e4814bb2dbf8bd2e9bc5b88c0b188acbfa862d37
spiekarski <sjp5vh@virginia.edu>
2019-03-03 18:15:28 -0500

    Testimonial wip

--- mainJS.js
+++ mainJS.js
@@ -1,11 +1,28 @@
 testSubmit = document.getElementById("testSubmit");
 testCancel = document.getElementById("testCancel")
 
-author = document.getElementById("author");
-anon = document.getElementById("anon");
-
 function submitTest() {
-    
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let testimonial = document.getElementById("testimonial").value;
+    if (title === '')
+    {
+        document.getElementById("title").focus();
+        document.getElementById("title-note").innerHTML = "Please provide a title";
+    }
+    else {
+        document.getElementById("title-note").innerHTML = "";
+        if (testimonial === '')
+        {
+            document.getElementById("test").focus();
+            document.getElementById("test-note").innerHTML = "Please provide your testimonial";
+        }
+        else {
+            document.getElementById("test-note").innerHTML = "";
+            var rowdata = [title, author, testimonial];
+        //    sent this rowdata to a database
+        }
+    }
     window.location.href = "testimonials.html";
 }
 
@@ -15,3 +32,17 @@ function cancel() {
 
 testCancel.addEventListener("click", cancel);
 testSubmit.addEventListener("click", submitTest);
+
+
+author = document.getElementById("author");
+anon = document.getElementById("anon");
+
+loadTestList = function(){
+    let card = document.createElement("div");
+    card.className = "card";
+    let body = document.createElement("div");
+    let title = document.createTextNode()
+}
+testList = document.getElementById("test-list");
+testList.addEventListener("load", loadTestList);
+
